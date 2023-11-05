51b242479c8b2111531f85208b6c6fdac5465e85
kevin arnold flores pacheco <arnold_k2001@hotmail.com>
2023-11-05 16:30:49 -0500

    section contact and logo customers finished

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 const navbar = document.querySelector('.navbar')
 const contact = document.querySelector('.contact-info')
+const navLinks = document.querySelectorAll('.navbar a')
+navLinks.forEach(link => {
+    link.addEventListener('click', (event) => {
+        event.preventDefault()
+        const targetId = link.getAttribute('href').substring(1)
+        const targetSection = document.getElementById(targetId)
+        navLinks.forEach(navLink => {
+            navLink.classList.remove('active')
+        })
+        link.classList.add('active')
+        targetSection.scrollIntoView({ behavior: 'smooth' })
+    })
+})
 
 document.querySelector('#menu-btn').onclick = () => {
     navbar.classList.toggle('active')
@@ -54,6 +67,30 @@ var swiper = new Swiper('.reviews-slider', {
         },
     },
 });
+var swiper = new Swiper('.logo-slider', {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 10,
+    speed: 6000,
+    autoplay: {
+        delay: 1,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        450: {
+            slidesPerView: 2,
+        },
+        640: {
+            slidesPerView: 3,
+        },
+        768: {
+            slidesPerView: 4,
+        },
+        1000: {
+            slidesPerView: 5,
+        },
+    },
+});
 
 let values = document.querySelectorAll('.num')
 let interval = 5000
@@ -64,7 +101,7 @@ values.forEach(value => {
     let duration = Math.floor(interval / endValue)
     let counter = setInterval(function () {
         startValue += 1
-        value.textContent = startValue
+        value.textContent = startValue + '+'
         if (startValue == endValue) {
             clearInterval(counter)
         }
